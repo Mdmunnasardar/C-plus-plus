59dd81c074bd81747b26ebf23cdccf216148ffee
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-06 19:28:16 +0600

    practice C++ problem

---
+++ datatype.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+ using namespace std;
+
+ int main(){
+    int a;
+    cin>>a;
+    long long l;
+    cin>>l;
+    char ch;
+    cin>>ch;
+    float f;
+    cin>>f;
+    double d;
+    cin>>d;
+    
+
+    cout<<a<<endl;
+    cout<<l<<endl;
+    cout<<ch<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+
+
+    return 0;
+ }
+ 
---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string S;
+    
+    getline(cin, S);
+
+    cout << "Hello, " << S << endl;
+    return 0;
+}
