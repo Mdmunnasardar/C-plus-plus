1f3e4eb14a2caf5203727dd37fee6e59233b89fe
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-07 17:05:31 +0600

    max mini by if else

--- K_Max_and_Min.cpp
+++ K_Max_and_Min.cpp
@@ -3,12 +3,34 @@ using namespace std;
 
 int main(){
     int A,B,C;
+    int Minimum;
+    int maximum;
     cin>>A>>B>>C;
 
-    if(A>B&&A>C)
-    cout<<A;
-    else 
-    cout<<"not";
+    if(A<=B&&A<=C){
+        Minimum=A;
+    }
+    else if(B<=A&&B<=C){
+        Minimum=B;
+    }
+    else{
+        Minimum=C;
+    
+    }
+
+    if (A>=B&&A>=C)
+    {
+        maximum=A;
+    }
+    else if(B>=A&&B>=C){
+        maximum=B; 
+     }
+
+    else{
+        maximum=C;
+    }
+
+    cout<<Minimum<<" "<<maximum;
 
     return 0;
 }
