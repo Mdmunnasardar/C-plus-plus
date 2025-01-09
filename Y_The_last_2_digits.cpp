6e41876ea67409d07f83f52fa2e66b4ad849e354
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-10 01:56:21 +0600

    using manipolators(setw,setfill)

--- Y_The_last_2_digits.cpp
+++ Y_The_last_2_digits.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <iomanip> 
 using namespace std;
 
 int main()
 {
-    long A, B, C, D;
+    long long A, B, C, D;
     cin >> A >> B >> C >> D;
 
-    long multi = A * B * C * D;
-    int rem = multi % 100;
+    
+    long long rem = 1;
+    rem = (rem * A) % 100;
+    rem = (rem * B) % 100;
+    rem = (rem * C) % 100;
+    rem = (rem * D) % 100;
 
-    cout << rem;
+    // Output the last two digits with leading zeros
+    cout << setw(2) << setfill('0') << rem << endl;
 
     return 0;
 }
