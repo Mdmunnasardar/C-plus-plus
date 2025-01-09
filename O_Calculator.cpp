c41047bc20deb865bb9670699b98f4cb5610d611
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-10 00:33:34 +0600

    switching /case use

--- O_Calculator.cpp
+++ O_Calculator.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int A, B;
     char S;
-    
+
     cin >> A >> S >> B;
-    
-   
-    switch (S) {
-        case '+':
-            cout << (A + B) << endl;
-            break;
-        case '-':
-            cout << (A - B) << endl;
-            break;
-        case '*':
-            cout << (A * B) << endl;
-            break;
-        case '/':
-            cout << (A / B) << endl;
-            break;
-        default:
-            cout << "Invalid operator" << endl;
+
+    switch (S)
+    {
+    case '+':
+        cout << (A + B) << endl;
+        break;
+    case '-':
+        cout << (A - B) << endl;
+        break;
+    case '*':
+        cout << (A * B) << endl;
+        break;
+    case '/':
+        cout << (A / B) << endl;
+        break;
+    default:
+        cout << "Invalid operator" << endl;
     }
-    
+
     return 0;
 }
