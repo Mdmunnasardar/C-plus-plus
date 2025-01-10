b1067b4e19f6feef8011541e4ee07f41d374205c
Md Munna Sardar <munnaofficialnubtk@gmail.com>
2025-01-10 23:28:07 +0600

    recheck

---
+++ T_Sort_Numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm> // For std::sort
+using namespace std;
+
+int main() {
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    int arr[] = {A, B, C};
+    std::sort(arr, arr + 3);
+
+    for (int i = 0; i < 3; i++) {
+        cout << arr[i] << endl;
+    }
+    cout << endl;
+    cout << A << endl;
+    cout << B << endl;
+    cout << C << endl;
+
+    return 0;
+}
--- U_Float_or_int.cpp
+++ U_Float_or_int.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main() {
+    float N;
+    cin >> N;  // Input the number
 
-double N;
-cin>>N;
-if (N)
+    int d = static_cast<int>(N);  // Get the integer part of N//also use int d=N;
+    float R = N - d;              // Get the decimal (fractional) part of N
 
+  
+    if (R == 0) {
+        cout << "int " << d << endl;  // If the decimal part is 0, print it as an integer
+    } else {
+        cout << "float " << d << " " << R << endl;  // Otherwise, print it as a float
+    }
 
-    cout <<endl;
     return 0;
 }
